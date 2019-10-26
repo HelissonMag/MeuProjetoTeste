722c83781b71d7169457ab20ac4553aeb52b840d
Ubuntu <ubuntu@ip-172-31-81-150.ec2.internal>
2019-10-26 00:31:36 +0000

    Meu primeiro commit

---
+++ src/main/java/br/ufs/dcomp/MeuProjeto/App.java
@@ -0,0 +1,12 @@
+package br.ufs.dcomp.MeuProjeto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+    }
+}
